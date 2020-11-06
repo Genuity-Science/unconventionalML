ca64b38ef868950efe31776304f85b19d9ae26be
Sharvari Gujja <sgujja@NextCODE.local@nc-hpc-027.idm.nextcode.local>
2020-11-06 19:05:03 +0000

    Removed folder from repository

--- unconventionalML/HCL/quantumtools/__init__.py
+++
@@ -1,8 +0,0 @@
-# quantumtools package, creates functions that are reused
-# should add this package to Python path for it to work properly, and be
-# importable from anywhere
-#
-# high to low level:
-# gates > qtools > unitary > accessories
-# if lower calls higher, it should do so within the function only,
-# else you get error from circular references
